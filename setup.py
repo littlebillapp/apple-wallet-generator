03a50eb1fa3d10686fb7ad27431db8a7336c6f8a
Geoffrey <geoffrey@littlebill.io>
2024-11-07 16:19:08 +0100

    fix(dependancies): rename folder

--- setup.py
+++ setup.py
@@ -1,29 +1,25 @@
 from setuptools import setup
 
-version = "1.0.0"
-
 setup(
     name="apple-wallet-generator",
-    version=version,
+    version="1.0.3",
+    description="A tool to generate Apple Wallet passes",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="Geoffrey Sautreau",
     author_email="geoffrey@littlebill.io",
-    packages=["apple-wallet-generator"],
     url="https://github.com/littlebillapp/apple-wallet-generator",
-    license=open("LICENSE.txt").read(),
-    description="Apple Wallet file generator",
-    long_description=open("README.md").read(),
+    packages=["apple_wallet_generator"],
     install_requires=[
+        "cffi==1.14.4",
+        "pycparser==2.20",
+        "six==1.15.0",
         "cryptography>=3.3.1",
     ],
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Other Environment",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3 :: Only",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    python_requires=">=3.6",
 )
